2972b5805a19016fe7ce7b2b382501f154f6669a
Veetaha <gerzoh1@gmail.com>
2019-09-01 17:05:24 +0300

    small refactoring

--- ambient-declarations/node-pre-gyp/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'node-pre-gyp' {
-
-    /** 
-     * Returns path to C++ native NodeJS addon based on package.json settings 
-     * set according to 'node-pre-gyp' requirements.
-     */
-    export function find(packageJsonPath: string): string;
-
-}
--- premake5.lua
+++ premake5.lua
@@ -6,26 +6,25 @@ workspace "lexertl"
 project "lexertl"
     language   "C++"
     buildoptions { 
-        "-std=gnu++2a" -- cppdialect doesn't support gnu++2a
+        "-std=gnu++2a" -- workaround because `cppdialect "gnu++2a"` is not allowed
     }
-    
-    -- Though we specify dynamic library traget, it will be linked statically, see:
-    -- https://emscripten.org/docs/compiling/Building-Projects.html#dynamic-linking
-    -- https://emscripten.org/docs/compiling/Building-Projects.html#archive-a-files
     kind    "ConsoleApp" 
 
-    targetname "%{prj.name}.js"
+    targetname "lexertl.js"
     location   "build/wasm"
     targetdir  "build/wasm/bin"
     objdir     "build/wasm/bin-int"
 
-    files { "src/**.cpp"}--, "vendor/fmt/build/libfmt.a" }
+    files { "src/**.cpp" }
     includedirs { 
         "src",
         "conan/modules/include",
         "vendor/*/include"
     }
-    libdirs { "conan/modules/lib", "vendor/fmt/build" }
+    libdirs { 
+        "conan/modules/lib", 
+        "vendor/fmt/build" 
+    }
     links { "fmt" }
 
     filter "configurations:Debug"
